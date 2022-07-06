2c92a24ae5ac1a697024257db4dfc3385705feb9
Jakub Wosiak <jakubwosiak99@gmail.com>
2022-07-06 14:23:09 +0200

    First commit

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Main.css"
+
+const Main = () => {
+  return (
+    <div className="app">
+      <h1>MlodyG Todo Portfolio</h1>
+    </div>
+  )
+}
+
+export default Main
